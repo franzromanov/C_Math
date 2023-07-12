bb2fbdfa4acc02c258d89b99091e9661a45c3b33
franzromanov <114895290+franzromanov@users.noreply.github.com>
2023-07-13 02:18:07 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
-cc -c -g C_Math.c && cc -c -g main.c && cd Vector && cc -c -g vector.c && cc -o main main.o C_Math.o ./Vector/vector.o
+#!/bin/bash
+cd ~/C_Math && cc -c -g C_Math.c && cc -c -g main.c && cd Vector && cc -c -g vector.c && cd .. && cc -o main main.o C_Math.o ./Vector/vector.o
