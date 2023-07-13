3e98ddaa8d32c9fe90fe6bb63f408283451ffeb4
franzromanov <114895290+franzromanov@users.noreply.github.com>
2023-07-13 22:18:48 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,62 @@
 #include "C_Math.h"
+//declare
 Vector P;
 Vector Q;
+ratio a;
+ratio b;
+ratio drop;
+int bag;
+
 Vector result;
 int main(){
-//code_here
+//initialize::Vector
 P=Vstream();
-//printf("\n");
 Q=Vstream();
+
+//process&output
 result=Vadd(P,Q);
-printf("the result is: ");
+printf("[additon] the result is: ");
 Vectorcat(result);
+
+//process&output
+result=Vsub(P,Q);
+printf("[subtraction] the result is: ");
+Vectorcat(result);
+
+//process&output
+result=dot(P,Q);
+printf("[dot product] the result is: ");
+Vectorcat(result);
+//////////////////////////////////////////
+
+//initialize::Rational_Number
+a=Rcat();
+b=Rcat();
+
+//rpocess&output
+drop=Radd(a,b);
+printf("[addition] the result is: ");
+Rout(drop);
+
+//rpocess&output
+drop=Rsub(a,b);
+printf("[subtraction] the result is: ");
+Rout(drop);
+
+//rpocess&output
+drop=R_incr(a);
+printf("[increment] the result is: ");
+Rout(drop);
+
+//rpocess&output
+drop=R_dcr(b);
+printf("[decrement] the result is: ");
+Rout(drop);
+
+//rpocess&output
+bag=R_comp(a,b);
+printf("[comparison] the result is: ");
+printf("%d\n",bag);
+
 return 0;
 }
