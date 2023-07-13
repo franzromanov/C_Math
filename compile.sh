594f280708484f371101baa2c0c0d0c0e19c6c71
franzromanov <114895290+franzromanov@users.noreply.github.com>
2023-07-13 15:53:03 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd ~/C_Math/sequence&series && cc -c -g sequence&series.c
-cd ~/C_Math && cc -c -g C_Math.c && cc -c -g main.c && cd Vector && cc -c -g vector.c && cd .. && cc -o main main.o C_Math.o ./Vector/vector.o ./sequence&series/sequence&series.o
+cd ~/C_Math/rational && cc -c -g ratio.c
+#cd ~/C_Math/sequence&series && cc -c -g sequence&series.c
+cd ~/C_Math && cc -c -g C_Math.c && cc -c -g main.c && cd Vector && cc -c -g vector.c && cd .. && cc -o main main.o C_Math.o ./Vector/vector.o ./rational/ratio.h
