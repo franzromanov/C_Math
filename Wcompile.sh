144ba8ffa4e5edcadeae7fb9d9f64ff23ea8638a
franzromanov <114895290+franzromanov@users.noreply.github.com>
2023-07-13 22:47:53 +0800

    Create Wcompile.sh

---
+++ Wcompile.sh
@@ -0,0 +1,7 @@
+
+
+
+#!/bin/bash
+cd ~/C_Math/rational && x86_64-w64-mingw64-gcc -c -g ratio.c
+#cd ~/C_Math/sequence&series && cc -c -g sequence&series.c
+cd ~/C_Math && x86_64-w64-mingw64-gcc -c -g C_Math.c && x86_64-w64-mingw64-gcc -c -g main.c && cd Vector && x86_64-w64-mingw64-gcc -c -g vector.c && cd .. && x86_64-w64-mingw64-gcc -o main main.o C_Math.o ./Vector/vector.o ./rational/ratio.o
